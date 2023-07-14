0ddd9c654e5b3915fc3cf8064aa3dcb193910195
zhouy <15955655592>
2023-07-15 02:19:05 +0800

    修改配置增加listener

---
+++ src/main/java/com/yukikyu/kook/boot/app/listener/KookListener.java
@@ -0,0 +1,10 @@
+package com.yukikyu.kook.boot.app.listener;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author: yukikyu
+ * @date: 2023-07-14 21:06
+ */
+@Component
+public class KookListener {}
